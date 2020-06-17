5cc7a6986c611d5039cb6412827c02da76105f56
will <sopwill3@gmail.com>
2020-06-17 15:08:31 -0600

    added blog list to Blog Posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -4,7 +4,9 @@ import styles from "./styles/posts.module.css"
 
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Posts(props) {
+
+  const data = props.data.allMarkdownRemark;
 
   return (
   <Layout>
@@ -13,6 +15,28 @@ export default function Home() {
     className={styles.header}
     >
         Hello Posts!
+    </div>
+
+    <div
+    className={styles.articleList}
+    >
+      <h3>
+        {data.totalCount} Posts
+      </h3>
+      {data.edges.map(({ node }) => (
+          <div 
+          className={styles.blogLink}
+          key={node.id}
+          >
+             <h2>
+               {node.frontmatter.title}
+             </h2>
+             <p>
+             {node.frontmatter.date}
+             </p>
+             <p>{node.excerpt}</p>
+          </div>
+        ))}
     </div>
 
   </Layout>
