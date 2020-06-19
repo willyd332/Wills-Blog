9feb6a99230956e94524efca42b791ebe18e51d7
will <sopwill3@gmail.com>
2020-06-18 22:01:01 -0600

    added Info-Tile

--- src/components/Info-Tile.js
+++ src/components/Info-Tile.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 export default function InfoTile(props) {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66];
+
   return (
-    <div>
-        InfoTile
+    <div className='container-fluid'>
+        <div className='row'>
+            <div className='col-12'>
+                Some helpful info about the blog and all the things that happen on the blog
+            </div>
+        </div>
+        <div className='row'>
+            {arr.map((num) => (
+                <div key={{ num }} className='col-12'>
+                    Recent Post # {num}
+                </div>
+            ))}
+        </div>
+        <div className='row'>
+            {arr.map((num) => (
+                <div key={{ num }} className='col-6'>
+                    Tag#{num}
+                </div>
+            ))}
+        </div>
     </div>
   );
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,12 +3,22 @@ import { useStaticQuery, Link, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import './styles/layout.css';
 import TitleTile from './Title-Tile';
+import InfoTile from './Info-Tile';
 
 export default function Layout(props) {
   return (
-    <div>
+    <div className='container-fluid'>
       <TitleTile imgUrl='https://cdn.spacetelescope.org/archives/images/wallpaper5/heic1502a.jpg' />
-      {props.children}
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-xl-10 col-lg-9' style={{ border: '1px solid black' }}>
+            {props.children}
+          </div>
+          <div className='col-lg-3 col-xl-2 d-none d-lg-block d-xl-block' style={{ border: '1px solid black' }}>
+            <InfoTile />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
