0588ea7aa5f894f4d1d773a606da07828c74257f
will <sopwill3@gmail.com>
2020-06-18 22:22:44 -0600

    added the Blog-Preview component and called it in index.js

--- src/components/Blog-Preview.js
+++ src/components/Blog-Preview.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import PropTypes from 'prop-types';
 
 export default function BlogPreview(props) {
+  const { postInfo } = props;
   return (
-    <div>
-        BlogPreview
+    <div className='container'>
+        <Link
+                to={postInfo.fields.slug}
+                className='row'
+         >
+            <div className='col-12'>
+                {postInfo.frontmatter.title}
+            </div>
+            <div className='col-12'>
+                This Will Be Image When I Figure It Out
+            </div>
+            <div className='col-12'>
+                {postInfo.excerpt}
+            </div>
+            <div className='col-2 offset-1'>
+                Other Info
+            </div>
+            <div className='col-2 offset-1'>
+                Other Info
+            </div>
+            <div className='col-2 offset-1'>
+                Other Info
+            </div>
+            <div className='col-2 offset-1'>
+                {postInfo.frontmatter.date}
+            </div>
+        </Link>
     </div>
   );
 }
+
+BlogPreview.propTypes = {
+  postInfo: PropTypes.shape({
+    id: PropTypes.string,
+    frontmatter: PropTypes.shape({
+      title: PropTypes.string,
+      date: PropTypes.string,
+    }),
+    fields: PropTypes.shape({
+      slug: PropTypes.string,
+    }),
+    excerpt: PropTypes.string,
+  }),
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,19 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import '../components/styles/bootstrap.css';
-import styles from '../components/styles/index.module.css';
 
 import Layout from '../components/layout';
+import BlogPreview from '../components/Blog-Preview';
 
 export default function Home(props) {
-  const first = props.data.allMarkdownRemark.edges[0].node;
+  const nodes = props.data.allMarkdownRemark.edges;
   return (
   <Layout>
-    <div>
-      <div>
-        <div>
-            Most Recent Post
-        </div>
-      </div>
-      <div className={'row'} >
-      <Link
-            to={first.fields.slug}
-          >
-             <h2>
-               {first.frontmatter.title}
-             </h2>
-             <p>
-             {first.frontmatter.date}
-             </p>
-             <p>
-             {first.excerpt}
-             </p>
-          </Link>
-      </div>
+    <div className='container-fluid'>
+      {nodes.map(({ node }) => (
+        <BlogPreview key={node.id} postInfo={node} />
+      ))}
     </div>
   </Layout>
   );
@@ -54,7 +37,7 @@ export const data = graphql`
           fields {
             slug
           }
-          excerpt
+          excerpt(pruneLength: 400)
         }
       }
     }
@@ -63,11 +46,6 @@ export const data = graphql`
 
 Home.propTypes = {
   data: PropTypes.shape({
-    site: PropTypes.shape({
-      siteMetadata: PropTypes.shape({
-        title: PropTypes.string,
-      }),
-    }),
     allMarkdownRemark: PropTypes.shape({
       totalCount: PropTypes.number,
       edges: PropTypes.arrayOf(PropTypes.shape({
